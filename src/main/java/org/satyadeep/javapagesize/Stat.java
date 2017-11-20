f4a47ab141930d96657419ee6521e1f3a7468d96
Satyadeep <satyasm@gmail.com>
2017-11-19 18:21:42 -0800

    stats sorted by time

--- src/main/java/org/satyadeep/javapagesize/Stat.java
+++ src/main/java/org/satyadeep/javapagesize/Stat.java
@@ -27,6 +27,9 @@ public class Stat {
     }
 
     public void toStdout() {
+        if (this.components.size() > 2) {
+            this.components.subList(2, this.components.size()).sort((c1, c2) -> (int)(c2.timeTaken - c1.timeTaken));
+        }
         System.out.printf("%-50s, %10d, %10d\n", trimToLength(this.url, 50), this.timeTaken, this.size);
         for (Stat s : this.components) {
             System.out.printf("  %-48s, %10d, %10d\n", trimToLength(s.url, 48), s.timeTaken, s.size);
